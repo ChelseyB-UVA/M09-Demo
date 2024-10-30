ff7dbdb547a78fb5c20d8e9b0ede7d16f660fa42
Chelsey <qck2qg@virginia.edu>
2024-10-29 20:19:33 -0400

    Trying to submit my participation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'Demo',
+    version = '0.1',
+    description = 'A simple demostration package',
+    url ='https://github.com/ChelseyB-UVA/M09-Demo.git'
+    author = 'Chelsey Blowe',
+    aurthor_email = 'qck2qg@virginia.edu',
+    license = 'MIT',
+    packages = ['demo']
+    )
